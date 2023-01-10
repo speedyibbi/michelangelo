ac1171458e0e67bb925ee48b14a664d7dadff706
Ibrahim Khan <ibbi2k2.khan@gmail.com>
2023-01-10 23:33:22 +0500

    fix: fix minor issues

--- client/src/components/UnityEngine.tsx
+++ client/src/components/UnityEngine.tsx
@@ -12,21 +12,21 @@ const UnityEngine = (): ReactElement => {
     codeUrl: '/game/game.wasm'
   })
 
-  useEffect(() => {
-    const getGame = async (): Promise<void> => {
-      const response = await fetch(`/game/file?title=${title !== undefined ? title : ''}`, { method: 'GET' })
-        .then(async (res) => await res.blob())
-        .then((blob) => new File([blob], 'webgl.zip'))
-        .then((file) => {
-          const fileReader = new FileReader()
-          fileReader.readAsArrayBuffer(file)
-          fileReader.onload = () => console.log(fileReader.result)
-        })
-        // .then((res) => res.headers.forEach(function (val, key) { console.log(key + ' -> ' + val) }))
-      // console.log(response)
-    }
-    void getGame()
-  }, [])
+  // useEffect(() => {
+  //   const getGame = async (): Promise<void> => {
+  //     const response = await fetch(`/game/file?title=${title !== undefined ? title : ''}`, { method: 'GET' })
+  //       .then(async (res) => await res.blob())
+  //       .then((blob) => new File([blob], 'webgl.zip'))
+  //       .then((file) => {
+  //         const fileReader = new FileReader()
+  //         fileReader.readAsArrayBuffer(file)
+  //         fileReader.onload = () => console.log(fileReader.result)
+  //       })
+  //       // .then((res) => res.headers.forEach(function (val, key) { console.log(key + ' -> ' + val) }))
+  //     // console.log(response)
+  //   }
+  //   void getGame()
+  // }, [])
 
   return (
     <Unity unityProvider={unityProvider} style={{ width: '100%', height: '100%', margin: '0', padding: '0' }} />
