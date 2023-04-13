46934776857784569beb6a6faadd59b3c5b7790c
Ibrahim Khan <ibbi2k2.khan@gmail.com>
2023-04-14 02:03:00 +0500

    feat: add loader

--- client/src/components/UnityEngine.tsx
+++ client/src/components/UnityEngine.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-tabs */
 import React, { ReactElement } from 'react'
 import { Unity, useUnityContext } from 'react-unity-webgl'
 
 const UnityEngine = (props: { game: string }): ReactElement => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: `/games/${props.game}/file?type=loader`,
     dataUrl: `/games/${props.game}/file?type=data`,
     frameworkUrl: `/games/${props.game}/file?type=framework`,
@@ -10,8 +11,19 @@ const UnityEngine = (props: { game: string }): ReactElement => {
   })
 
   return (
-    <Unity unityProvider={unityProvider}
-    style={{ width: '100%', height: '100%', margin: '0', padding: '0' }} />
+		<>
+			{!isLoaded &&
+				<div className='flex flex-col items-center justify-center w-full h-full'>
+					<span className='w-20 h-20 border-2 rounded-full border-b-secondary border-neutral-600 animate-spin' />
+					<p className='m-10 text-white'>
+						{Math.round(loadingProgression * 100)}%
+					</p>
+				</div>}
+			<Unity
+				unityProvider={unityProvider}
+				style={{ width: '100%', height: '100%', margin: '0', padding: '0', visibility: isLoaded ? 'visible' : 'hidden' }}
+			/>
+		</>
   )
 }
 
